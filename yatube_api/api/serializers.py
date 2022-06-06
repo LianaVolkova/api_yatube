386c472cbc7b87386d88458ecbbeaa6537d10d6f
Liana Volkova <99835375+LianaVolkova@users.noreply.github.com>
2022-06-06 19:15:19 +0300

    Update serializers.py

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 from rest_framework.relations import SlugRelatedField
 
-
 from posts.models import Comment, Group, Follow, Post, User
 
 
